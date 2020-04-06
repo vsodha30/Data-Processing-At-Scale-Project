37fab5689239ba99efb5718e71353391c436587c
Dhaval <dhaval0024@gmail.com>
2020-04-05 18:31:50 -0700

    Phase 1

---
+++ Group_7_CSE511_Phase1/src/main/scala/cse512/SpatialQuery.scala
@@ -0,0 +1,86 @@
+package cse512
+
+import org.apache.spark.sql.SparkSession
+
+object SpatialQuery extends App{
+
+  def st_contains(rectangle: String, point: String): Boolean = {
+    val Array(rectx1, recty1, rectx2, recty2) = rectangle.split(",").map(x => x.toDouble);
+    val Array(pointx, pointy) = point.split(",").map(x => x.toDouble);
+    if(pointx >= rectx1 && pointx <= rectx2 && pointy >= recty1 && pointy <= recty2) {
+      return true
+    }
+    return false;
+  }
+
+  def st_within(point1: String, point2: String, distance:Double): Boolean = {
+    val Array(point1x, point1y) = point1.split(",").map(x => x.toDouble);
+    val Array(point2x, point2y) = point2.split(",").map(x => x.toDouble);
+    val dist = ((point1x - point2x) * (point1x - point2x)) + ((point1y - point2y) * (point1y - point2y));
+    if(dist <= distance) {
+      return true;
+    }
+    return false;
+  }
+
+  def runRangeQuery(spark: SparkSession, arg1: String, arg2: String): Long = {
+
+    val pointDf = spark.read.format("com.databricks.spark.csv").option("delimiter","\t").option("header","false").load(arg1);
+    pointDf.createOrReplaceTempView("point")
+
+    // YOU NEED TO FILL IN THIS USER DEFINED FUNCTION
+    spark.udf.register("ST_Contains",(queryRectangle:String, pointString:String)=>((st_contains(queryRectangle, pointString))))
+
+    val resultDf = spark.sql("select * from point where ST_Contains('"+arg2+"',point._c0)")
+    resultDf.show()
+
+    return resultDf.count()
+  }
+
+  def runRangeJoinQuery(spark: SparkSession, arg1: String, arg2: String): Long = {
+
+    val pointDf = spark.read.format("com.databricks.spark.csv").option("delimiter","\t").option("header","false").load(arg1);
+    pointDf.createOrReplaceTempView("point")
+
+    val rectangleDf = spark.read.format("com.databricks.spark.csv").option("delimiter","\t").option("header","false").load(arg2);
+    rectangleDf.createOrReplaceTempView("rectangle")
+
+    // YOU NEED TO FILL IN THIS USER DEFINED FUNCTION
+    spark.udf.register("ST_Contains",(queryRectangle:String, pointString:String)=>((st_contains(queryRectangle, pointString))))
+
+    val resultDf = spark.sql("select * from rectangle,point where ST_Contains(rectangle._c0,point._c0)")
+    resultDf.show()
+
+    return resultDf.count()
+  }
+
+  def runDistanceQuery(spark: SparkSession, arg1: String, arg2: String, arg3: String): Long = {
+
+    val pointDf = spark.read.format("com.databricks.spark.csv").option("delimiter","\t").option("header","false").load(arg1);
+    pointDf.createOrReplaceTempView("point")
+
+    // YOU NEED TO FILL IN THIS USER DEFINED FUNCTION
+    spark.udf.register("ST_Within",(pointString1:String, pointString2:String, distance:Double)=>((st_within(pointString1, pointString2, distance))))
+
+    val resultDf = spark.sql("select * from point where ST_Within(point._c0,'"+arg2+"',"+arg3+")")
+    resultDf.show()
+
+    return resultDf.count()
+  }
+
+  def runDistanceJoinQuery(spark: SparkSession, arg1: String, arg2: String, arg3: String): Long = {
+
+    val pointDf = spark.read.format("com.databricks.spark.csv").option("delimiter","\t").option("header","false").load(arg1);
+    pointDf.createOrReplaceTempView("point1")
+
+    val pointDf2 = spark.read.format("com.databricks.spark.csv").option("delimiter","\t").option("header","false").load(arg2);
+    pointDf2.createOrReplaceTempView("point2")
+
+    // YOU NEED TO FILL IN THIS USER DEFINED FUNCTION
+    spark.udf.register("ST_Within",(pointString1:String, pointString2:String, distance:Double)=>((st_within(pointString1, pointString2, distance))))
+    val resultDf = spark.sql("select * from point1 p1, point2 p2 where ST_Within(p1._c0, p2._c0, "+arg3+")")
+    resultDf.show()
+
+    return resultDf.count()
+  }
+}
