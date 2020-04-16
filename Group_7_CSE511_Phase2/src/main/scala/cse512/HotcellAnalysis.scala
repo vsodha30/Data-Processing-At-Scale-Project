f8008daadb2b44d7e16f22b5287a2421334beb7c
Dhaval <dhaval0024@gmail.com>
2020-04-16 00:14:44 -0700

    hotcell analysis added

--- Group_7_CSE511_Phase2/src/main/scala/cse512/HotcellAnalysis.scala
+++ Group_7_CSE511_Phase2/src/main/scala/cse512/HotcellAnalysis.scala
@@ -31,7 +31,11 @@ def runHotcellAnalysis(spark: SparkSession, pointPath: String): DataFrame =
   pickupInfo = spark.sql("select CalculateX(nyctaxitrips._c5),CalculateY(nyctaxitrips._c5), CalculateZ(nyctaxitrips._c1) from nyctaxitrips")
   var newCoordinateName = Seq("x", "y", "z")
   pickupInfo = pickupInfo.toDF(newCoordinateName:_*)
+
+
+  pickupInfo.createOrReplaceTempView("input")
   pickupInfo.show()
+
 
   // Define the min and max of x, y, z
   val minX = -74.50/HotcellUtils.coordinateStep
@@ -42,8 +46,37 @@ def runHotcellAnalysis(spark: SparkSession, pointPath: String): DataFrame =
   val maxZ = 31
   val numCells = (maxX - minX + 1)*(maxY - minY + 1)*(maxZ - minZ + 1)
 
-  // YOU NEED TO CHANGE THIS PART
+  pickupInfo.filter("x >= " + minX + " AND x <= " + maxX)
+  pickupInfo.filter("y >= " + minY + " AND y <= " + maxY)
+  pickupInfo.filter("z >= " + minZ + " AND z <= " + maxZ)
 
-  return pickupInfo // YOU NEED TO CHANGE THIS PART
+  spark.udf.register("is_neighbour", (x1:Int, x2:Int, x3:Int, y1:Int, y2:Int, y3:Int)=>(HotcellUtils.isNeighbour(x1,x2,x3,y1,y2,y3)))
+  spark.udf.register("gvalue", (x: Int, y: Int, z: Int, attr: Int, ncount: Int, neighbours:Int, xmean: Double, s: Double, numCells: Int)=>(HotcellUtils.gvalue(x, y, z, attr, ncount, neighbours, xmean, s, numCells)))
+  spark.udf.register("total_neighbours", (x: Int, y: Int, z: Int, minX: Int, maxX: Int, minY: Int, maxY: Int, minZ: Int, maxZ: Int) => (HotcellUtils.total_neighbours(x, y, z, minX, maxX, minY, maxY, minZ, maxZ)))
+  // YOU NEED TO CHANGE THIS PART
+  val attrDF = pickupInfo.groupBy("x","y","z").count().as("aggr")
+  attrDF.createOrReplaceTempView("attr_table")
+  val xmean = spark.sql("SELECT count(attr_table.count) FROM attr_table").first().getLong(0).toDouble / numCells
+  val s = math.sqrt((spark.sql("SELECT count(power(attr_table.count, 2)) FROM attr_table").first().getLong(0).toDouble / numCells) - (xmean * xmean))
+  attrDF.show()
+
+  val neighboursDf = spark.sql("SELECT t1.x as x, t1.y as y, t1.z as z, t1.count as attr, t2.x as nx, t2.y as ny, t2.z as nz, t2.count as nattr FROM attr_table as t1, attr_table as t2 WHERE is_neighbour(t1.x, t1.y, t1.z, t2.x, t2.y, t2.z)")
+  neighboursDf.createOrReplaceTempView("processed_table")
+  neighboursDf.show()
+
+  val bounds = minX + ", " + maxX + ", " + minY + ", " + maxY + ", " + minZ + ", " + maxZ
+  val aggrDF = spark.sql("SELECT x, y, z, attr, sum(nattr) as ncount, total_neighbours(x, y, z, " + bounds + ") as neighbours FROM processed_table GROUP BY x,y,z,attr")
+  //sum(power(ncount, 2)) as ncount2,
+  aggrDF.createOrReplaceTempView("aggr_table")
+  aggrDF.show()
+
+
+
+  val global_param = xmean + ", " + s + ", " + numCells
+  val gDF = spark.sql("SELECT x, y, z, gvalue(x, y, z, attr, ncount, neighbours, " + global_param + ") as g FROM aggr_table ORDER BY g DESC")
+  //, ncount2
+  gDF.show()
+
+  return gDF // YOU NEED TO CHANGE THIS PART
 }
 }
--- Group_7_CSE511_Phase2/src/main/scala/cse512/HotcellUtils.scala
+++ Group_7_CSE511_Phase2/src/main/scala/cse512/HotcellUtils.scala
@@ -47,5 +47,48 @@ object HotcellUtils {
     return calendar.get(Calendar.DAY_OF_MONTH)
   }
 
+  def isNeighbour (x1: Int, x2: Int, x3: Int, y1:Int, y2:Int, y3:Int): Boolean = {
+    if(x1 == y1 + 1 || x1 == y1 - 1) {
+      if(x2 == y2 + 1 || x2 == y2 - 1) {
+        if(x3 == y3 + 1 || x3 == y3 - 1) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  def total_neighbours(x: Int, y: Int, z: Int, minX: Int, maxX: Int, minY: Int, maxY: Int, minZ: Int, maxZ: Int): Int = {
+    var corners = 0;
+    if(x == minX || x == maxX) {
+      corners += 1;
+    }
+    if(y == minY || y == maxY) {
+      corners += 1;
+    }
+    if(z == minZ || z == maxZ) {
+      corners += 1;
+    }
+    if(corners == 0) {
+      return 26;
+    }
+    else if(corners == 1) {
+      return 17;
+    }
+    else if(corners == 2) {
+      return 11;
+    }
+    return 7;
+  }
+
+  def gvalue(x: Int, y: Int, z: Int, attr: Int, ncount: Int, neighbours: Int, xmean: Double, s: Double, numCells: Int): Double = {
+    /*val nxmean = ncount.toDouble / neighbours.toDouble
+    val ns = math.sqrt((ncount2 / neighbours) - (nxmean * nxmean))
+    val nnum = ncount - (nxmean * neighbours)
+    val ndeno = ns * math.sqrt(((neighbours * neighbours) - (neighbours * neighbours)) / (numCells - 1))*/
+    val num = ncount - (xmean * neighbours)
+    val deno = s * math.sqrt(((numCells * neighbours) - (neighbours * neighbours)) / (numCells - 1))
+    return num / deno;
+  }
   // YOU NEED TO CHANGE THIS PART
 }
